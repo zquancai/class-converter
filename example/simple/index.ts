35500899cfffde481ca722ae1b2f959481eaf991
zhengqc <zhengqc@seagroup.com>
2019-12-08 00:36:43 +0800

    fix: del example

--- example/simple/index.ts
+++
@@ -1,6 +0,0 @@
-import { toClass } from '../../src';
-import UserModel from './user';
-import userJson from './user.json';
-
-const json = userJson;
-console.log(toClass<UserModel>(json, UserModel));
